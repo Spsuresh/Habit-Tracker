d7c0b9c824f6a81eb8b891f832bd89c474c49407
spsuresh <spsuri777@gmail.com>
2023-04-16 22:26:06 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import { App } from 'app';
-import { AppProviders } from 'context';
+import App from '../app';
+import AppProviders  from '../context';
 import reportWebVitals from './reportWebVitals';
-import 'fontsource-roboto/300.css';
-import 'fontsource-roboto/400.css';
-import 'fontsource-roboto/500.css';
-import 'fontsource-roboto/700.css';
+import '../node_modules/@fontsource/roboto/300.css';
+import '../node_modules/@fontsource/roboto/400.css';
+import '../node_modules/@fontsource/roboto/500.css'
+import '../node_modules/@fontsource/roboto/700.css';
 
 render(
   <AppProviders>
